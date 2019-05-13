6444b26d5215b08dab6aadc4ff381617cbaed729
mameAmy466 <48662077+mameAmy466@users.noreply.github.com>
2019-05-13 13:10:32 +0000

    Add files via upload

---
+++ photo.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>les photos</title>
+<style>
+        .image{
+            width:30%;
+            height:30%;
+       }
+ </style>
+</head>
+<body>
+    <div class="container-flud">
+    <header>
+      <?php 
+     include"nav.php";
+     ?>
+       </header>
+    </div>
+    <div class="container">
+    <div class="shadow-lg p-3 mb-5 rounded">
+       <div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/3.jpeg">
+        <img src="images/images/3.jpeg" alt="Lights" style="width:100%" class="img-circle image">
+        <div class="caption">
+          <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/h1.jpeg">
+        <img src="images/images/h1.jpeg" alt="Nature" style="width:100%" class="img-circle image">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/e1.jpeg">
+        <img src="images/images/e1. Brand - G3, Product code - G3-GSS0573, Price - 4280, Color - Grey, Fabric - Silk," alt="Fjords" style="width:100%" class="img-circle image">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+   <div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/e3.jpeg">
+        <img src="images/images/e3.jpeg" alt="Lights" style="width:100%">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/e2.jpeg">
+        <img src="images/images/e2.jpeg" alt="Nature" style="width:100%">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/h2.jpeg">
+        <img src="images/images/h2.jpeg" alt="Fjords" style="width:100%">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/t1.jpeg">
+        <img src="images/images/t1.jpeg" alt="Lights" style="width:100%">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/t2.jpeg">
+        <img src="images/images/t2.jpeg" alt="Nature" style="width:100%">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="images/images/t3.jpeg">
+        <img src="images/images/t3.jpeg" alt="Fjords" style="width:100%">
+        <div class="caption">
+        <input type="text"  placeholder="Enter votre appreciation">
+          <button class="btn btn-info">Envoyer</button>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+<div class="container flud">
+<footer class="container-flud bg-info">
+ <?php 
+include"footer.php";
+?>
+</div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="js/pop.js"></script>
+
+</body>
+</html>
